ca77b2dd120b04c33d21f2f5b0224c7a42717aa1
mikk-c <michele.coscia@gmail.com>
2013-01-15 15:24:23 -0500

    Initialized models.py

--- django_files/explore/models.py
+++ django_files/explore/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+class Entity(models.Model):
+   name = models.CharField(max_length = 256)
+   COUNTRY = 'CO'
+   ORGANIZATION = 'OR'
+   ISSUE = 'IS'
+   TYPE_OF_ENTITY_CHOICES = (
+      (COUNTRY, 'Country'),
+      (ORGANIZATION, 'Organization'),
+      (ISSUE, 'Issue'),
+   )
+   type_of_entity = models.CharField(max_length = 2, choices = TYPE_OF_ENTITY_CHOICES)
+   relations = models.ManyToManyField('self', through = 'Bipartite', symmetrical = False)
+
+class Bipartite_manager(models.Manager):
+   pass
+
+class Bipartite(models.Model):
+   entity_src = models.ForeignKey(Entity)
+   entity_trg = models.ForeignKey(Entity)
+   hits = models.IntegerField()
+   rca = models.FloatField()
+   alpha = models.FloatField()
+   beta = models.FloatField()
