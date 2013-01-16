a872df8fdb12061842e982551a3384671bb954ae
mikk-c <michele.coscia@gmail.com>
2013-01-16 13:28:48 -0600

    Template fix

---
+++ django_files/aid_explorer/settings_local.py
@@ -0,0 +1,21 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'aidxp_db',                      # Or path to database file if using sqlite3.
+        'USER': 'root',                      # Not used with sqlite3.
+        'PASSWORD': '1n7ugrzy',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+    }
+}
+
+STATICFILES_DIRS = (
+    '/srv/www/aid_explorer/media',
+)
+
+SECRET_KEY = '-%vx@ocghp%7a3+@6evdzn#=v071$t-cl6m&4h5^iv#65mt13y'
+
+TEMPLATE_DIRS = (
+    '/srv/www/aid_explorer/html',
+)
+
