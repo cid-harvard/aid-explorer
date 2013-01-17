3e99b6c369f3c93ee8059747c21f9560a37953e6
mikk-c <michele.coscia@gmail.com>
2013-01-17 14:36:00 -0500

    Fixtures updated

--- data/django_input_generator.py
+++ data/django_input_generator.py
@@ -6,24 +6,24 @@ issues = []
 
 f = open("activity_list", 'r')
 for line in f:
-   issues.append(line.strip())
+   issues.append(line.strip().capitalize())
 f.close()
 
 f = open("concern_list", 'r')
 for line in f:
-   issues.append(line.strip())
+   issues.append(line.strip().capitalize())
 f.close()
 
 countries = []
 f = open("country_list", 'r')
 for line in f:
-   countries.append(line.strip())
+   countries.append(line.strip().capitalize())
 f.close()
 
 orgs = []
 f = open("institution_list", 'r')
 for line in f:
-   orgs.append(line.strip())
+   orgs.append(line.strip().capitalize())
 f.close()
 
 json_list = []
@@ -67,55 +67,55 @@ beta = defaultdict(lambda : defaultdict(float))
 f = open("20120501country_issue_poweregressions_anneal", 'r')
 for line in f:
    fields = line.strip().split('\t')
-   alpha[entity_id[fields[0]]][entity_id[fields[1]]] = float(fields[2])
-   alpha[entity_id[fields[1]]][entity_id[fields[0]]] = float(fields[2])
-   beta[entity_id[fields[0]]][entity_id[fields[1]]] = float(fields[3])
-   beta[entity_id[fields[1]]][entity_id[fields[0]]] = float(fields[3])
+   alpha[entity_id[fields[0].capitalize()]][entity_id[fields[1].capitalize()]] = float(fields[2])
+   alpha[entity_id[fields[1].capitalize()]][entity_id[fields[0].capitalize()]] = float(fields[2])
+   beta[entity_id[fields[0].capitalize()]][entity_id[fields[1].capitalize()]] = float(fields[3])
+   beta[entity_id[fields[1].capitalize()]][entity_id[fields[0].capitalize()]] = float(fields[3])
 f.close()
 
 f = open("20120501country_organization_poweregressions_anneal_2", 'r')
 for line in f:
    fields = line.strip().split('\t')
-   alpha[entity_id[fields[0]]][entity_id[fields[1]]] = float(fields[2])
-   alpha[entity_id[fields[1]]][entity_id[fields[0]]] = float(fields[2])
-   beta[entity_id[fields[0]]][entity_id[fields[1]]] = float(fields[3])
-   beta[entity_id[fields[1]]][entity_id[fields[0]]] = float(fields[3])
+   alpha[entity_id[fields[0].capitalize()]][entity_id[fields[1].capitalize()]] = float(fields[2])
+   alpha[entity_id[fields[1].capitalize()]][entity_id[fields[0].capitalize()]] = float(fields[2])
+   beta[entity_id[fields[0].capitalize()]][entity_id[fields[1].capitalize()]] = float(fields[3])
+   beta[entity_id[fields[1].capitalize()]][entity_id[fields[0].capitalize()]] = float(fields[3])
 f.close()
 
 f = open("20120501organization_issue_poweregressions_anneal_2", 'r')
 for line in f:
    fields = line.strip().split('\t')
-   alpha[entity_id[fields[0]]][entity_id[fields[1]]] = float(fields[2])
-   alpha[entity_id[fields[1]]][entity_id[fields[0]]] = float(fields[2])
-   beta[entity_id[fields[0]]][entity_id[fields[1]]] = float(fields[3])
-   beta[entity_id[fields[1]]][entity_id[fields[0]]] = float(fields[3])
+   alpha[entity_id[fields[0].capitalize()]][entity_id[fields[1].capitalize()]] = float(fields[2])
+   alpha[entity_id[fields[1].capitalize()]][entity_id[fields[0].capitalize()]] = float(fields[2])
+   beta[entity_id[fields[0].capitalize()]][entity_id[fields[1].capitalize()]] = float(fields[3])
+   beta[entity_id[fields[1].capitalize()]][entity_id[fields[0].capitalize()]] = float(fields[3])
 f.close()
 
 f = open("20120501country_issue_rca_aidweb_2", 'r')
 for line in f:
    fields = line.strip().split('\t')
-   hits[entity_id[fields[0]]][entity_id[fields[1]]] = int(fields[2])
-   hits[entity_id[fields[1]]][entity_id[fields[0]]] = int(fields[2])
-   rca[entity_id[fields[0]]][entity_id[fields[1]]] = float(fields[3])
-   rca[entity_id[fields[1]]][entity_id[fields[0]]] = float(fields[3])
+   hits[entity_id[fields[0].capitalize()]][entity_id[fields[1].capitalize()]] = int(fields[2])
+   hits[entity_id[fields[1].capitalize()]][entity_id[fields[0].capitalize()]] = int(fields[2])
+   rca[entity_id[fields[0].capitalize()]][entity_id[fields[1].capitalize()]] = float(fields[3])
+   rca[entity_id[fields[1].capitalize()]][entity_id[fields[0].capitalize()]] = float(fields[3])
 f.close()
 
 f = open("20120501org_country_rca_aidweb", 'r')
 for line in f:
    fields = line.strip().split('\t')
-   hits[entity_id[fields[0]]][entity_id[fields[1]]] = int(fields[2])
-   hits[entity_id[fields[1]]][entity_id[fields[0]]] = int(fields[2])
-   rca[entity_id[fields[0]]][entity_id[fields[1]]] = float(fields[3])
-   rca[entity_id[fields[1]]][entity_id[fields[0]]] = float(fields[3])
+   hits[entity_id[fields[0].capitalize()]][entity_id[fields[1].capitalize()]] = int(fields[2])
+   hits[entity_id[fields[1].capitalize()]][entity_id[fields[0].capitalize()]] = int(fields[2])
+   rca[entity_id[fields[0].capitalize()]][entity_id[fields[1].capitalize()]] = float(fields[3])
+   rca[entity_id[fields[1].capitalize()]][entity_id[fields[0].capitalize()]] = float(fields[3])
 f.close()
 
 f = open("20120501org_issue_rca_aidweb", 'r')
 for line in f:
    fields = line.strip().split('\t')
-   hits[entity_id[fields[0]]][entity_id[fields[1]]] = int(fields[2])
-   hits[entity_id[fields[1]]][entity_id[fields[0]]] = int(fields[2])
-   rca[entity_id[fields[0]]][entity_id[fields[1]]] = float(fields[3])
-   rca[entity_id[fields[1]]][entity_id[fields[0]]] = float(fields[3])
+   hits[entity_id[fields[0].capitalize()]][entity_id[fields[1].capitalize()]] = int(fields[2])
+   hits[entity_id[fields[1].capitalize()]][entity_id[fields[0].capitalize()]] = int(fields[2])
+   rca[entity_id[fields[0].capitalize()]][entity_id[fields[1].capitalize()]] = float(fields[3])
+   rca[entity_id[fields[1].capitalize()]][entity_id[fields[0].capitalize()]] = float(fields[3])
 f.close()
 
 n = 1
@@ -131,5 +131,28 @@ for entity1 in hits:
       n += 1
 
 f = open("fixture_bipartite.json", 'w')
+f.write(json.dumps(json_list))
+f.close()
+
+rank = {}
+value = {}
+f = open("rankings", 'r')
+for line in f:
+   fields = line.strip().split('\t')
+   rank[entity_id[fields[1].capitalize()]] = fields[0]
+   value[entity_id[fields[1].capitalize()]] = fields[2]
+f.close()
+
+n = 1
+json_list = []
+for entity in rank:
+   record = {}
+   record["model"] = "explore.ranking"
+   record["pk"] = n
+   record["fields"] = {"entity": entity, "rank": rank[entity], "consistency_value": value[entity]}
+   json_list.append(record)
+   n += 1
+
+f = open("fixture_rankings.json", 'w')
 f.write(json.dumps(json_list))
 f.close()
