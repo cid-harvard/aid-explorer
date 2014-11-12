473e088b633e00322abd9963de08f6efc72d1698
Mali Akmanalp <mali@akmanalp.com>
2014-11-12 17:24:51 -0500

    Add prefix to settings

--- django_files/aid_explorer/settings_local.py
+++ django_files/aid_explorer/settings_local.py
@@ -1,3 +1,5 @@
+PREFIX = '/Users/makmana/aidxp'
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
@@ -6,12 +8,12 @@ DATABASES = {
 }
 
 STATICFILES_DIRS = (
-    '/srv/www/media/aid_explorer',
+    PREFIX + '/media',
 )
 
 SECRET_KEY = '-%vx@ocghp%7a3+@6evdzn#=v071$t-cl6m&4h5^iv#65mt13y'
 
 TEMPLATE_DIRS = (
-    '/srv/www/aid_explorer/html',
+    PREFIX + '/html',
 )
 
