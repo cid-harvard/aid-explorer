56638eb51881593491441aef0938c8878746b5c2
Mali Akmanalp <mali@akmanalp.com>
2014-11-12 17:17:00 -0500

    Switch db to sqlite3

--- django_files/aid_explorer/settings_local.py
+++ django_files/aid_explorer/settings_local.py
@@ -1,11 +1,7 @@
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'aidxp_db',                      # Or path to database file if using sqlite3.
-        'USER': 'root',                      # Not used with sqlite3.
-        'PASSWORD': '1n7ugrzy',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'aidxp_db.sqlite3',                      # Or path to database file if using sqlite3.
     }
 }
 
